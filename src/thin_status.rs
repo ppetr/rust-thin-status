fe14e547e121676786a0be593c9f01f685e063b0
Petr <petr.mvd+git@gmail.com>
2026-06-25 10:36:22 +0200

    Remove an obosolete TODO

--- src/thin_status.rs
+++ src/thin_status.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Add a feature for google_cloud_rpc::model::Status;
 #[cfg(feature = "use_cloud_rpc")]
 use google_cloud_rpc::model as cloud_rpc;
 use std::error::Error;
