7f7ef77029f3e3afba59e805fedf1393de5a9f36
Petr <petr.mvd+git@gmail.com>
2026-06-25 10:41:46 +0200

    Clean up the `ErrorCode` documentation to reflect Rust code instead of the original C++ constants.

--- src/status_code.rs
+++ src/status_code.rs
@@ -43,150 +43,118 @@ use strum;
 #[repr(i32)]
 #[non_exhaustive]
 pub enum ErrorCode {
-    /// StatusCode::kCancelled
-    ///
-    /// kCancelled (gRPC code "CANCELLED") indicates the operation was cancelled, typically by the
+    /// `Cancelled` (gRPC code "CANCELLED") indicates the operation was cancelled, typically by the
     /// caller.
     Cancelled = 1,
 
-    /// StatusCode::kUnknown
-    ///
-    /// kUnknown (gRPC code "UNKNOWN") indicates an unknown error occurred. In general, more
+    /// `Unknown` (gRPC code "UNKNOWN") indicates an unknown error occurred. In general, more
     /// specific errors should be raised, if possible. Errors raised by APIs that do not return
     /// enough error information may be converted to this error.
     Unknown = 2,
 
-    /// StatusCode::kInvalidArgument
-    ///
-    /// kInvalidArgument (gRPC code "INVALID_ARGUMENT") indicates the caller specified an invalid
+    /// `InvalidArgument` (gRPC code "INVALID_ARGUMENT") indicates the caller specified an invalid
     /// argument, such as a malformed filename. Note that use of such errors should be narrowly
     /// limited to indicate the invalid nature of the arguments themselves. Errors with validly
     /// formed arguments that may cause errors with the state of the receiving system should be
-    /// denoted with `kFailedPrecondition` instead.
+    /// denoted with `FailedPrecondition` instead.
     InvalidArgument = 3,
 
-    /// StatusCode::kDeadlineExceeded
-    ///
-    /// kDeadlineExceeded (gRPC code "DEADLINE_EXCEEDED") indicates a deadline expired before the
+    /// `DeadlineExceeded` (gRPC code "DEADLINE_EXCEEDED") indicates a deadline expired before the
     /// operation could complete. For operations that may change state within a system, this error
     /// may be returned even if the operation has completed successfully. For example, a successful
     /// response from a server could have been delayed long enough for the deadline to expire.
     DeadlineExceeded = 4,
 
-    /// StatusCode::kNotFound
-    ///
-    /// kNotFound (gRPC code "NOT_FOUND") indicates some requested entity (such as a file or
+    /// `NotFound` (gRPC code "NOT_FOUND") indicates some requested entity (such as a file or
     /// directory) was not found.
     ///
-    /// `kNotFound` is useful if a request should be denied for an entire class of users, such as
+    /// `NotFound` is useful if a request should be denied for an entire class of users, such as
     /// during a gradual feature rollout or undocumented allow list. If a request should be denied
     /// for specific sets of users, such as through user-based access control, use
-    /// `kPermissionDenied` instead.
+    /// `PermissionDenied` instead.
     NotFound = 5,
 
-    /// StatusCode::kAlreadyExists
-    ///
-    /// kAlreadyExists (gRPC code "ALREADY_EXISTS") indicates that the entity a caller attempted to
+    /// `AlreadyExists` (gRPC code "ALREADY_EXISTS") indicates that the entity a caller attempted to
     /// create (such as a file or directory) is already present.
     AlreadyExists = 6,
 
-    /// StatusCode::kPermissionDenied
-    ///
-    /// kPermissionDenied (gRPC code "PERMISSION_DENIED") indicates that the caller does not have
+    /// `PermissionDenied` (gRPC code "PERMISSION_DENIED") indicates that the caller does not have
     /// permission to execute the specified operation. Note that this error is different than an
     /// error due to an *un*authenticated user. This error code does not imply the request is valid
     /// or the requested entity exists or satisfies any other pre-conditions.
     ///
-    /// `kPermissionDenied` must not be used for rejections caused by exhausting some resource.
-    /// Instead, use `kResourceExhausted` for those errors. `kPermissionDenied` must not be used if
-    /// the caller cannot be identified. Instead, use `kUnauthenticated` for those errors.
+    /// `PermissionDenied` must not be used for rejections caused by exhausting some resource.
+    /// Instead, use `ResourceExhausted` for those errors. `PermissionDenied` must not be used if
+    /// the caller cannot be identified. Instead, use `Unauthenticated` for those errors.
     PermissionDenied = 7,
 
-    /// StatusCode::kResourceExhausted
-    ///
-    /// kResourceExhausted (gRPC code "RESOURCE_EXHAUSTED") indicates some resource has been
+    /// `ResourceExhausted` (gRPC code "RESOURCE_EXHAUSTED") indicates some resource has been
     /// exhausted, perhaps a per-user quota, or perhaps the entire file system is out of space.
     ResourceExhausted = 8,
 
-    /// StatusCode::kFailedPrecondition
-    ///
-    /// kFailedPrecondition (gRPC code "FAILED_PRECONDITION") indicates that the operation was
+    /// `FailedPrecondition` (gRPC code "FAILED_PRECONDITION") indicates that the operation was
     /// rejected because the system is not in a state required for the operation's execution. For
     /// example, a directory to be deleted may be non-empty, an "rmdir" operation is applied to a
     /// non-directory, etc.
     ///
     /// Some guidelines that may help a service implementer in deciding between
-    /// `kFailedPrecondition`, `kAborted`, and `kUnavailable`:
+    /// `FailedPrecondition`, `Aborted`, and `Unavailable`:
     ///
-    ///  (a) Use `kUnavailable` if the client can retry just the failing call.
-    ///  (b) Use `kAborted` if the client should retry at a higher transaction level (such as when a
+    ///  (a) Use `Unavailable` if the client can retry just the failing call.
+    ///  (b) Use `Aborted` if the client should retry at a higher transaction level (such as when a
     ///      client-specified test-and-set fails, indicating the client should restart a
     ///      read-modify-write sequence).
-    ///  (c) Use `kFailedPrecondition` if the client should not retry until the system state has
+    ///  (c) Use `FailedPrecondition` if the client should not retry until the system state has
     ///      been explicitly fixed. For example, if a "rmdir" fails because the directory is
-    ///      non-empty, `kFailedPrecondition` should be returned since the client should not retry
+    ///      non-empty, `FailedPrecondition` should be returned since the client should not retry
     ///      unless the files are deleted from the directory.
     FailedPrecondition = 9,
 
-    /// StatusCode::kAborted
-    ///
-    /// kAborted (gRPC code "ABORTED") indicates the operation was aborted, typically due to a
+    /// `Aborted` (gRPC code "ABORTED") indicates the operation was aborted, typically due to a
     /// concurrency issue such as a sequencer check failure or a failed transaction.
     ///
-    /// See the guidelines above for deciding between `kFailedPrecondition`, `kAborted`, and
-    /// `kUnavailable`.
+    /// See the guidelines above for deciding between `FailedPrecondition`, `Aborted`, and
+    /// `Unavailable`.
     Aborted = 10,
 
-    /// StatusCode::kOutOfRange
-    ///
-    /// kOutOfRange (gRPC code "OUT_OF_RANGE") indicates the operation was attempted past the valid
+    /// `OutOfRange` (gRPC code "OUT_OF_RANGE") indicates the operation was attempted past the valid
     /// range, such as seeking or reading past an end-of-file.
     ///
-    /// Unlike `kInvalidArgument`, this error indicates a problem that may be fixed if the system
-    /// state changes. For example, a 32-bit file system will generate `kInvalidArgument` if asked
-    /// to read at an offset that is not in the range [0,2^32-1], but it will generate `kOutOfRange`
+    /// Unlike `InvalidArgument`, this error indicates a problem that may be fixed if the system
+    /// state changes. For example, a 32-bit file system will generate `InvalidArgument` if asked
+    /// to read at an offset that is not in the range [0,2^32-1], but it will generate `OutOfRange`
     /// if asked to read from an offset past the current file size.
     ///
-    /// There is a fair bit of overlap between `kFailedPrecondition` and `kOutOfRange`.  We
-    /// recommend using `kOutOfRange` (the more specific error) when it applies so that callers who
-    /// are iterating through a space can easily look for an `kOutOfRange` error to detect when they
-    /// are done.
+    /// There is a fair bit of overlap between `FailedPrecondition` and `OutOfRange`.  We recommend
+    /// using `OutOfRange` (the more specific error) when it applies so that callers who are
+    /// iterating through a space can easily look for an `OutOfRange` error to detect when they are
+    /// done.
     OutOfRange = 11,
 
-    /// StatusCode::kUnimplemented
-    ///
-    /// kUnimplemented (gRPC code "UNIMPLEMENTED") indicates the operation is not implemented or
+    /// `Unimplemented` (gRPC code "UNIMPLEMENTED") indicates the operation is not implemented or
     /// supported in this service. In this case, the operation should not be re-attempted.
     Unimplemented = 12,
 
-    /// StatusCode::kInternal
-    ///
-    /// kInternal (gRPC code "INTERNAL") indicates an internal error has occurred and some
+    /// `Internal` (gRPC code "INTERNAL") indicates an internal error has occurred and some
     /// invariants expected by the underlying system have not been satisfied. This error code is
     /// reserved for serious errors.
     Internal = 13,
 
-    /// StatusCode::kUnavailable
-    ///
-    /// kUnavailable (gRPC code "UNAVAILABLE") indicates the service is currently unavailable and
+    /// `Unavailable` (gRPC code "UNAVAILABLE") indicates the service is currently unavailable and
     /// that this is most likely a transient condition. An error such as this can be corrected by
     /// retrying with a backoff scheme. Note that it is not always safe to retry non-idempotent
     /// operations.
     ///
-    /// See the guidelines above for deciding between `kFailedPrecondition`, `kAborted`, and
-    /// `kUnavailable`.
+    /// See the guidelines above for deciding between `FailedPrecondition`, `Aborted`, and
+    /// `Unavailable`.
     Unavailable = 14,
 
-    /// StatusCode::kDataLoss
-    ///
-    /// kDataLoss (gRPC code "DATA_LOSS") indicates that unrecoverable data loss or corruption has
+    /// `DataLoss` (gRPC code "DATA_LOSS") indicates that unrecoverable data loss or corruption has
     /// occurred. As this error is serious, proper alerting should be attached to errors such as
     /// this.
     DataLoss = 15,
 
-    /// StatusCode::kUnauthenticated
-    ///
-    /// kUnauthenticated (gRPC code "UNAUTHENTICATED") indicates that the request does not have
+    /// `Unauthenticated` (gRPC code "UNAUTHENTICATED") indicates that the request does not have
     /// valid authentication credentials for the operation. Correct the authentication and try
     /// again.
     Unauthenticated = 16,
